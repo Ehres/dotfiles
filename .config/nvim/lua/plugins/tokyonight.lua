e56301db1fa8572d02c6184600186863a3e94481
Maxime GREBAUVAL <maxime.grebauval@manomano.com>
2026-02-10 11:31:54 +0100

    refactor(tokyonight.nvim): remove transparency

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,24 +1,3 @@
 return {
   "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "dark",
-    },
-    on_highlights = function(hl, c)
-      hl.CursorLineNr = {
-        fg = c.orange,
-        bold = true,
-      }
-      hl.LineNrAbove = {
-        fg = c.cyan,
-        bold = false,
-      }
-      hl.LineNrBelow = {
-        fg = c.cyan,
-        bold = false,
-      }
-    end,
-  },
 }
