5b35a0e210ba718deaf419ccd95117abc491c44a
Maxime GREBAUVAL <maxime.grebauval@manomano.com>
2026-02-18 11:47:08 +0100

    feat(raycast): add raycast timer integration

---
+++ .config/raycast/scripts/timer-stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Stop Timer
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🍅
+# @raycast.packageName SketchyBar Timer
+
+~/.config/sketchybar/plugins/timer_ctl.sh stop
