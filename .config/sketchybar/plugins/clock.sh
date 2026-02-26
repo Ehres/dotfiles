c6e61a9f292851d78a76b4b285fc88c280688ab2
Maxime GREBAUVAL <maxime.grebauval@manomano.com>
2026-02-26 17:52:21 +0100

    refactor(sketchybar): update de dispay of clock

--- .config/sketchybar/plugins/clock.sh
+++ .config/sketchybar/plugins/clock.sh
@@ -4,5 +4,5 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+sketchybar --set "$NAME" label="$(LC_TIME=fr_FR.UTF-8 date '+%a %d %b %H:%M')"
 
