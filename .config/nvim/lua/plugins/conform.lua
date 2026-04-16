9e961ccebe551ff9872efb73861e116eebc1a608
Maxime GREBAUVAL <maxime.grebauval@Host-001.lan>
2026-04-16 14:27:43 +0200

    feat(nvim): add conform config for oxfmt

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs({ "javascript", "javascriptreact", "typescript", "typescriptreact" }) do
+        opts.formatters_by_ft[ft] = { "oxfmt", "prettier", stop_after_first = true }
+      end
+    end,
+  },
+}
