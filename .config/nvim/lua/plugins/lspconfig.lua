bf3bdc7b3d85968db0ccae5c3c1d0a408001cd3f
Maxime Grébauval <maxime.grebauval@icloud.com>
2026-08-31 17:49:41 +0200

    fix(nvim): use native typescript lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,5 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      tsc = { enabled = true, mason = false },
+      vtsls = { enabled = false },
+    },
   },
 }
